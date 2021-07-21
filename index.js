7913c353d3c1ff13052385d873a85ccf9b2ac635
rinlama <rinjinlama83@gmail.com>
2021-07-21 19:43:53 -0400

    init node

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+const socketRouter = require("./routes/socketRouter")(io).router;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/v1", socketRouter);
+
+//  app request
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// app api request
+// app.get("/api", socketRouter);
+
+// socket
+
+// io.on("connection", (socket) => {
+//   console.log(socket.id);
+
+//   socket.on("chat", (msg) => {
+//     io.emit("chat", msg);
+//   });
+// });
+
+server.listen(3001, () => {
+  console.log("Server started");
+});
---
+++ routes/socketRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+function socketRouter(io) {
+  const router = express.Router();
+
+  const emitSocket = () => {
+    // io.on("connection", (socket) => {
+    //   console.log(socket.id);
+    // });
+
+    router.post("/emitlog", (req, res) => {
+      console.log(req.body);
+      // io.emit("chat", req.body);
+      res.send("test");
+    });
+  };
+
+  emitSocket();
+
+  return {
+    router,
+  };
+}
+
+module.exports = socketRouter;
